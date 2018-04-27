9142ad093829014fc7ceb73d86567340a6b475a0
Rafemo <rafemo1718daw2@gmail.com>
2018-04-27 20:09:26 +0200

    Commit 1 de nifphp2

--- nifphp2/index.php
+++ nifphp2/index.php
@@ -4,14 +4,32 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<?php
+	function calc_nif($dni) {
+            $NUM=23;
+            $taula=array('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E');
+	    return  $taula[$dni % $NUM];
+	}
+	if (isset($_POST["dni"])){
+	   $dni=$_POST["dni"];
+	   $nif=calc_nif($dni);
+	   echo "<b><u>RESULTAT DEL CÀLCUL DE LA LLETRA DEL NIF A PARTIR DEL DNI:</u></b><br>";
+	   echo "DNI: ".$dni."<br>";
+	   echo "Lletra NIF: ".$nif."<br>";
+	   echo "<br>";
+	   echo "Desenvolupador: Raul Fernandez Moreno";
+	}
+?>
+</html>
+	<head>
+		<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+		<title>FORMULARI PEL CÀLCUL DE LA LLETRA DEL NIF A PARTIR DEL DNI</title>	
+	</head>  
+	<body>
+		<b>FORMULARI PEL CÀLCUL DE LA LLETRA DEL NIF A PARTIR DEL DNI</b><br><br>
+		<form name=frm_combo action="index.php" method="POST">
+				DNI: <input type="text" name="dni"><br><br>
+				<input type="submit" value="Envia"/><input type="reset" value="Esborra"/>
+		</form>		
+	</body>
 </html>
